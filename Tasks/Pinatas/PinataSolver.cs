fc47177155e2884028975b7f8ddbf772bf3cb21e
ARTWIN25\marya <maryanaivanina8@gmail.com>
2026-06-24 16:22:50 +0300

    Added logic for determing the max amount of candies

---
+++ Tasks/Pinatas/PinataSolver.cs
@@ -0,0 +1,46 @@
+﻿namespace Pinatas
+{
+    internal class PinataSolver
+    {
+        private int[] _extendedPinatas;
+        public int CalculateMaxAmount(int[] pinatas)
+        {
+            int n = pinatas.Length;
+
+            _extendedPinatas = new int[n + 2];
+
+            _extendedPinatas[0] = 1;
+            _extendedPinatas[n + 1] = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                _extendedPinatas[i + 1] = pinatas[i];
+            }
+
+            return GetMaxAmount(0, n + 1);
+        }
+
+        private int GetMaxAmount(int left, int right)
+        {
+            if (left + 1 == right)
+                return 0;
+
+            int maxAmount = 0;
+
+            for (int i = left + 1; i < right; i++)
+            {
+                int currentCandidate = _extendedPinatas[left] * _extendedPinatas[i] * _extendedPinatas[right];
+
+                int leftPart = GetMaxAmount(left, i);
+                int rightPart = GetMaxAmount(i, right);
+
+                int total = currentCandidate + leftPart + rightPart;
+
+                if (total > maxAmount)
+                    maxAmount = total;
+            }
+
+            return maxAmount;
+        }
+    }
+}
--- Tasks/Pinatas/Program.cs
+++ Tasks/Pinatas/Program.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int result = 0;
+
+            Console.WriteLine("Enter an array of pinatas separated by spaces:");
+            string input = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                string[] splitedPinatasString = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                int[] pinatas = new int[splitedPinatasString.Length];
+
+                for (int i = 0; i < splitedPinatasString.Length; i++)
+                {
+                    if (!int.TryParse(splitedPinatasString[i], out pinatas[i]))
+                    {
+                        Console.WriteLine($"Error! Please enter only numbers.");
+                        return;
+                    }
+                }
+
+                PinataSolver solver = new PinataSolver();
+                result = solver.CalculateMaxAmount(pinatas);
+
+
+                Console.WriteLine($"Max amount of candies: {result}");
+            }
+            else
+                Console.WriteLine("An empty line was entered.");
         }
     }
 }
